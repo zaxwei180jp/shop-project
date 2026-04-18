864212d38e7f78d1bf9ecc660704dc0cdabcd4fb
zaxwei180jp <zaxwei180jp@gmail.com>
2026-04-18 21:01:51 +0900

    Add files via upload

--- api/products.js
+++ api/products.js
@@ -20,18 +20,19 @@ export default async function handler(req, res) {
     return {
       id: page.id,
 
-      name: props.name?.title[0]?.plain_text || "",
+      name: props.name?.title?.[0]?.plain_text || "",
 
       price: props.price?.number || 0,
 
-      image: props.image?.url || "",
+      // ⭐ 修正這裡
+      image: props.image?.url || props.image?.rich_text?.[0]?.plain_text || "",
 
       category: props.category?.select?.name || "",
 
-      description: props.description?.rich_text[0]?.plain_text || "",
+      description: props.description?.rich_text?.[0]?.plain_text || "",
 
-      // ⭐ 多圖（關鍵）
-      images: props.images?.rich_text[0]?.plain_text
+      // ⭐ 多圖安全版
+      images: props.images?.rich_text?.[0]?.plain_text
         ? props.images.rich_text[0].plain_text.split(",")
         : []
     };
