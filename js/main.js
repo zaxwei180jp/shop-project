467d4bdff33851b32b038538d578c90fd3e43bbd
zaxwei180jp <zaxwei180jp@gmail.com>
2026-04-18 16:39:09 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const SUPABASE_URL = "https://khbtzvdzdyahstlaszbe.supabase.co";
+const SUPABASE_KEY = "sb_publishable_AmwMoCJ-OQUShoPQoEH1Fw_Y-6qmQpQ";
+
+fetch(`${SUPABASE_URL}/rest/v1/products`, {
+  headers: {
+    apikey: SUPABASE_KEY,
+    Authorization: `Bearer ${SUPABASE_KEY}`
+  }
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log("商品資料:", data);
+
+    const list = document.getElementById("product-list");
+    list.innerHTML = "";
+
+    data.forEach(p => {
+      list.innerHTML += `
+        <div class="bg-white p-4 rounded-2xl shadow">
+          <img src="${p.image}" class="rounded-xl mb-3">
+          <h3 class="font-bold">${p.name}</h3>
+          <p class="text-gray-500">NT$${p.price}</p>
+        </div>
+      `;
+    });
+  });
