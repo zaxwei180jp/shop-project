ca39e2e612ed784263cca9c4a4cdd458cd7dab4d
zaxwei180jp <zaxwei180jp@gmail.com>
2026-05-02 00:10:54 +0900

    Add files via upload

--- api/products.js
+++ api/products.js
@@ -19,40 +19,51 @@ export default async function handler(req, res) {
     const products = data.results.map((page) => {
       const props = page.properties;
 
+      // ⭐ 安全圖片處理
+      const cover =
+        page.cover?.external?.url ||
+        page.cover?.file?.url ||
+        "";
+
+      const images =
+        props.images?.files?.map(f =>
+          f.external?.url || f.file?.url
+        ) || [];
+
+      const finalImages =
+        images.length > 0
+          ? images
+          : cover
+            ? [cover]
+            : ["https://via.placeholder.com/400"];
+
       return {
         id: page.id,
 
-        // 名稱
         name: props.tname?.title?.[0]?.plain_text || "無名稱",
 
-        // 價格
         price: props.tprice?.number || 0,
 
-        // 原價（特價用）
         originalPrice: props.originalPrice?.number || 0,
 
-        // 分類
         category: props.category?.select?.name || "",
 
-        // ⭐ 新商品
         isNew: props.isNew?.checkbox || false,
 
-        // ⭐ 特價
         isSale: props.isSale?.checkbox || false,
 
-        // ⭐🔥 熱賣（你缺這個）
-        hot: props.hot?.checkbox || false,
+        // ⭐🔥 熱賣（容錯）
+        hot:
+          props.hot?.checkbox ??
+          props.Hot?.checkbox ??
+          props.isHot?.checkbox ??
+          false,
 
-        // 圖片
-        image: page.cover?.external?.url || page.cover?.file?.url || "",
+        image: finalImages[0],
+        images: finalImages,
 
-        // 多圖
-        images: props.images?.files?.map(f =>
-          f.external?.url || f.file?.url
-        ) || [],
-
-        // 描述
-        description: props.description?.rich_text?.[0]?.plain_text || "",
+        description:
+          props.description?.rich_text?.[0]?.plain_text || "",
 
         createdTime: page.created_time,
         update: props.update?.date?.start || page.created_time,
@@ -62,6 +73,7 @@ export default async function handler(req, res) {
     res.status(200).json(products);
 
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 }
