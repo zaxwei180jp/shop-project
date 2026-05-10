1d6fe4a448d276585c1021b9f00061b0e54438e3
zaxwei180jp <zaxwei180jp@gmail.com>
2026-05-11 00:15:04 +0900

    Update products.js

--- api/products.js
+++ api/products.js
@@ -79,7 +79,7 @@ export default async function handler(req, res) {
       const props = page.properties;
 
       const isSale = getCheckbox(props.Sale);
-      const price = getNumber(props.tpric);
+      const price = getNumber(props.tprice);
       const sprice = getNumber(props.sprice);
 
       return {
